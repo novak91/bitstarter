9c7653c62a320f5c79f10d271973dc1760d1a677
Radek Novak <novak.radek@gsystem.sk>
2013-07-10 22:06:33 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require(fs);
 
 var app = express.createServer(express.logger());
 
@@ -6,7 +7,7 @@ var app = express.createServer(express.logger());
 var index_data = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send(index_data);
+  response.send(index_data());
 });
 
 var port = process.env.PORT || 5000;
