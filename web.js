c35e98ab5318409f6acb2b19938837a170bdae41
Ubuntu <ubuntu@ip-10-34-229-79.eu-west-1.compute.internal>
2013-07-19 21:44:05 +0000

    Opened port 8080 web.js

--- web.js
+++ web.js
@@ -3,12 +3,15 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/js'));
+
 app.get('/', function(request, response) {
     var index_data = fs.readFileSync('index.html');
     response.send(index_data.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
